db3ec256768f730394bb6248046a3a76439b2257
Draghici Andrei <draghici.andrei095@gmail.com>
2021-05-28 18:50:34 +0300

    Initial commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,7 @@
+// Note: se pot utiliza și getters/setters dar aici folosim direct câmpurile publice.
+public class Student {
+    public String name;
+    public int age;
+
+    // Metodele de GET si SET pot fi si ele adaugate in momentul in care dorim sa ne facem campurile private.
+}
